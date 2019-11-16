6a9a41e8cc596e3f80bfe94fffe920b1ad945400
lijs <lijs@citex.io>
2019-11-16 11:00:53 +0800

    add .gitignore file

--- router/index.js
+++ router/index.js
@@ -1,5 +1,5 @@
 const routers = require('express').Router();
 const block_demo = require('./block_demo');
-routers.post('/block_demo',block_demo)
+routers.get('/block_demo',block_demo)
 
 module.exports = routers;
